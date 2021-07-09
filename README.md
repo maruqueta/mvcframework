ae1432ebf2c4c373fd0714df39072b2c7453eeff
Maria Eugenia Aguiar <maruqueta@gmail.com>
2021-07-09 14:55:13 -0300

    estructura

---
+++ README.md
@@ -0,0 +1,5 @@
+# PHP y MySQL con Docker - Para el pueblo!
+
+Instalar docker, docker-compose, clonar este repo, y tirar esto en la consola:
+`docker-compose up -d`
+
---
+++ app/controllers/Pages.php
@@ -0,0 +1,9 @@
+<?php
+class Pages extends Controller{
+    public function __construct() {
+    }
+    public function index() {
+       echo "home page";
+    }
+
+}
